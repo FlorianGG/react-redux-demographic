7bc69c5f0827a08bf29af61ff1229b7210cec840
Florian Garcia <florian.prof@gmail.com>
2018-08-19 12:00:13 +0200

    installation package react-chartkick with highCharts adapatater

--- src/components/MortalityListItem.js
+++ src/components/MortalityListItem.js
@@ -1,11 +1,19 @@
+import Highcharts from 'highcharts';
 import React from 'react';
+import ReactChartkick, { ColumnChart } from 'react-chartkick';
 
 import Flag from './Flag';
+
+ReactChartkick.addAdapter(Highcharts)
+
+const xTitle = 'Âge';
+const yTitle = '% de mortalité';
 
 const MortalityListItem = () => {
   return (
     <div>
       <Flag country="France" className="flag-medium"/>
+      <ColumnChart xtitle={xTitle} ytitle={yTitle} data={[["Sun", 32], ["Mon", 46], ["Tue", 28]]}/>
     </div>
   );
 };
